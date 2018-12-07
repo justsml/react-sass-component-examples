02b7951d80706c0b52d84e2d325e52ebeb06a02a
Dan Levy <397632+justsml@users.noreply.github.com>
2018-12-07 16:00:26 -0700

    README and organization fixes

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
 # Component Oriented SASS in React!
 
-
 > Are you not sold on `CSS-in-JS` options? 
 > Or feel it is overused?
 
-This project is for you.
+**This project is for you!**
 
-We will explore how **nested classes** in SCSS (or similar) can **help organize your presentation design.**
+## Summary
 
-Here's an example of how a Bootstrap modal could be modified:
+This project demonstrates how **nested classes** in SCSS (Less, Stylus or something similar) can **help organize your presentation design.**
+
+## How does this work?
+
+Here's how to create _Parent Class Modifiers_ which extend the base Bootstrap Modal behavior:
+
+#### There's 2 parts to this: HTML & (S)CSS.
+
+> First, let's look at the example Modal HTML:
+
+```html
+<div class="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">...</div>
+      <div class="modal-body">Modal body text goes here.</div>
+      <div class="modal-footer">
+        <button>A button</button>
+      </div>
+    </div>
+  </div>
+</div>
+```
+
+> Now, for the secret sauce... Some Sass:
 
 ```scss
 .modal-dialog {
@@ -27,6 +50,10 @@ Here's an example of how a Bootstrap modal could be modified:
     max-width: 80%;
   }
 }
-
 ```
+
+### Important!
+
+**Note:** Only 1 selector (`.modal-dialog`) is used to contain **all related permutations (or modifiers)** for any component.
+
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles.scss";
 
+const CatCallingCard = ({width = 250, height = 250}) => (
+  <div className='card mx-auto cat-box' style={{ width: (width + 50) }}>
+    <h5 class="card-header text-left">
+      Because every demo needs a cat photo
+    </h5>
+    <img className='card-img-bottom' src={`https://adorbs-as-a-service.herokuapp.com/api/v1/${width}w/${height}h/`} alt='necessary random pic of my kitty ♥️' />
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +21,7 @@ function App() {
       <p>
         <a href='https://github.com/justsml/react-sass-component-examples' target='_blank'>View project on GitHub</a>
         <br />
-        This is a demo of Component-oriented Sass. We will define & use some modifiers to affect how a Bootstrap Modal appears.
+        This is a demo of <b>Component-oriented Sass</b>. We'll define & use some modifiers to affect how a Bootstrap Modal appears.
         <br />
         <b>List of Modal modifiers:</b> <i>.dark-mode</i>, <i>.no-header</i>, <i>.spaced-buttons</i> and <i>.fluid-width</i>.
 
@@ -22,11 +31,11 @@ function App() {
           <h3>Single Modifiers</h3>
           <MyModal buttonLabel={`Open Modal (Base CSS)`} />
           <br />
-          <MyModal className="dark-mode" buttonLabel={`Open Modal (CSS Modifier: .dark-mode)`} />
+          <MyModal className="dark-mode" buttonLabel={`Open Modal (CSS Modifier: .dark-mode)`}><CatCallingCard /></MyModal>
           <br />
-          <MyModal className="no-header" buttonLabel={`Open Modal (CSS Modifier: .no-header)`} />
+          <MyModal className="no-header" buttonLabel={`Open Modal (CSS Modifier: .no-header)`}><CatCallingCard /></MyModal>
           <br />
-          <MyModal className="fluid-width" buttonLabel={`Open Modal (CSS Modifier: .fluid-width)`} />
+          <MyModal className="fluid-width" buttonLabel={`Open Modal (CSS Modifier: .fluid-width)`}><CatCallingCard width={550} height={375} /></MyModal>
         </div>
         <div className='col-6'>
           <h3>Combined Modifiers</h3>
@@ -35,10 +44,21 @@ function App() {
           <MyModal className="no-header spaced-buttons" buttonLabel={`Open Modal (.spaced-buttons & .no-header)`} buttonColor='warning' />
           <br />
           <MyModal className="spaced-buttons no-header fluid-width" buttonLabel={`.spaced-buttons, .no-header & .fluid-width`} buttonColor='warning' />
+          <br />
+          <MyModal className="dark-mode spaced-buttons fluid-width" buttonLabel={`.dark-mode, .spaced-buttons  & .fluid-width`} buttonColor='warning'>
+            <CatCallingCard width={550} height={325} />
+          </MyModal>
         </div>
       </div>
+
+
+      <div className='row' style={{marginBottom: '100px'}}>
+        <CatCallingCard width={410} height={360} />
+      </div>
+
       <nav className='navbar fixed-bottom navbar-dark bg-dark text-white'>
-        By: <a className='text-white' href='https://danlevy.net/' target='_blank'>Dan Levy</a>
+        <label>Created By:&#160;<a className='text-white' title='visit my blog' href='https://danlevy.net/' target='_blank'>Dan Levy</a></label>
+        <label>Tweet me:&#160;<a className='text-white' title='tweeter me' href='https://twitter.com/justsml' target='_blank'>@justsml</a></label>
       </nav>
     </div>
   );
